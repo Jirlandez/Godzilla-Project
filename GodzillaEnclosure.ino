48e6d747b6cfe606625a893b60d76f5c906a43ad
Jirlandez <71806357+Jirlandez@users.noreply.github.com>
2025-11-05 13:27:56 -0600

    Comments in GodzillaEnclosure.ino

--- GodzillaEnclosure.ino
+++ GodzillaEnclosure.ino
@@ -1,11 +1,26 @@
 //Godzilla Enclosure
+// Julian 1-2
 
+// Rodolfo 3-4
+
+// Connor 5-6
+
+// Code that runs once
 void setup() {
-  // put your setup code here, to run once:
+// Julian Setup 1-2
+
+// Rodolfo Setup 3-4
+
+// Connor Setup 5-6
 
 }
 
+// Code that runs repeatedly
 void loop() {
-  // put your main code here, to run repeatedly:
+// Julian Loop 1-2
+
+// Rodolfo Loop 3-4
+
+// Connor Loop 5-6
 
 }
