1cc3e98842b36543e91fe4f108a3471417aa0114
Jirlandez <71806357+Jirlandez@users.noreply.github.com>
2025-11-05 13:36:53 -0600

    Update GodzillaEnclosure.ino

--- GodzillaEnclosure.ino
+++ GodzillaEnclosure.ino
@@ -1,5 +1,6 @@
 //Godzilla Enclosure
 // Julian 1-2
+Servo doorservo;
 
 // Rodolfo 3-4
 
@@ -8,6 +9,7 @@
 // Code that runs once
 void setup() {
 // Julian Setup 1-2
+doorservo.attach(#) // Replace '#' with pin number
 
 // Rodolfo Setup 3-4
 
@@ -18,7 +20,8 @@ void setup() {
 // Code that runs repeatedly
 void loop() {
 // Julian Loop 1-2
-
+doorservo.write(val) // Replace with speed we decide for servo
+  
 // Rodolfo Loop 3-4
 
 // Connor Loop 5-6
