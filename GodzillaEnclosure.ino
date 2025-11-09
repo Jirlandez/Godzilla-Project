46b94b8b12cbaca590c52116a422c0e4f8c7ee6a
RodolfoMartinezM <rm66882@my.utexas.edu>
2025-11-09 16:53:03 -0600

    Added detailed descriptions for panels 3-4
    - Added an explanation of the events taking place in each scene.
    - Added the required Arduino materials that will be used.

--- GodzillaEnclosure.ino
+++ GodzillaEnclosure.ino
@@ -1,11 +1,35 @@
 //Godzilla Enclosure
+// Julian 1-2
+Servo doorservo;
 
+// Rodolfo 3-4
+
+// Connor 5-6
+
+// Code that runs once
 void setup() {
-  // put your setup code here, to run once:
+// Julian Setup 1-2
+doorservo.attach(#) // Replace '#' with pin number
+
+// Rodolfo Setup 3-4
+/* Panel 3: Godzilla dodges attack.
+Soldier motioning direction by waving arm - (1) 180 servo
+Godzilla is manully moved from point A (left) to point B (right) - (1) Copper tape Switch */
+/* Panel 4: Godzilla Charges beam.
+Pressing crystal cutouts (batteries) on Godzilla's back) - (3) Copper tape switch
+Light emited by godzilla's mouth changes - (1) LED light */
+
+// Connor Setup 5-6
 
 }
 
+// Code that runs repeatedly
 void loop() {
-  // put your main code here, to run repeatedly:
+// Julian Loop 1-2
+doorservo.write(val) // Replace with speed we decide for servo
+  
+// Rodolfo Loop 3-4
+
+// Connor Loop 5-6
 
 }
